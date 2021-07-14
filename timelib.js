58e65859f551aba69445851f9bbeb5b380ec7a83
RG2N <64289295+RG2N@users.noreply.github.com>
2021-07-14 14:24:13 -0400

    Update timelib.js

--- timelib.js
+++ timelib.js
@@ -2,4 +2,13 @@ function msToS(milliseconds) {
   return milliseconds / 1000;
 }
 
-module.exports = {msToS}; // Functions
+function todaysDate() {
+  var datetime = new Date();
+  return datetime;
+}
+
+function formatDate(today) {
+  return today.toISOString().split('T')[0];
+}
+
+module.exports = {msToS, todaysDate, formatDate}; // Functions
